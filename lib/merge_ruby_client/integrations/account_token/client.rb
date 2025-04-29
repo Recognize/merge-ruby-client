be8ac3f799561e12441da83c324356eb38318a13
Rajan Bhattarai <cdrrazan@gmail.com>
2025-04-29 18:59:32 +0545

    feat: Implement account_token for merge integration to the gem

---
+++ lib/merge_ruby_client/integrations/account_token/client.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require_relative "../../../requests"
+require_relative "../types/account_token"
+require "async"
+
+module Merge
+  module Integrations
+    class AccountTokenClient
+      # @return [Merge::RequestClient]
+      attr_reader :request_client
+      
+      # @param request_client [Merge::RequestClient]
+      # @return [Merge::Integrations::AccountTokenClient]
+      def initialize(request_client:)
+        @request_client = request_client
+      end
+      
+      # Returns the account token for the end user with the provided public token.
+      #
+      # @param public_token [String]
+      # @param request_options [Merge::RequestOptions]
+      # @return [Merge::Integrations::AccountToken]
+      # @example
+      #  api = Merge::Client.new(
+      #    base_url: "https://api.example.com",
+      #    environment: Merge::Environment::PRODUCTION,
+      #    api_key: "YOUR_AUTH_TOKEN"
+      #  )
+      #  api.integrations.account_token.retrieve(public_token: "public_token")
+      def retrieve(public_token:, request_options: nil)
+        response = @request_client.conn.get do |req|
+          req.options.timeout = request_options.timeout_in_seconds unless request_options&.timeout_in_seconds.nil?
+          req.headers["Authorization"] = request_options.api_key unless request_options&.api_key.nil?
+          req.headers["X-Account-Token"] = request_options.account_token unless request_options&.account_token.nil?
+          req.headers = {
+            **(req.headers || {}),
+            **@request_client.get_headers,
+            **(request_options&.additional_headers || {})
+          }.compact
+          unless request_options.nil? || request_options&.additional_query_parameters.nil?
+            req.params = { **(request_options&.additional_query_parameters || {}) }.compact
+          end
+          unless request_options.nil? || request_options&.additional_body_parameters.nil?
+            req.body = { **(request_options&.additional_body_parameters || {}) }.compact
+          end
+          req.url "#{@request_client.get_url(request_options: request_options)}/integrations/account-token/#{public_token}"
+        end
+
+        Merge::Integrations::AccountToken.from_json(json_object: response.body)
+      end
+    end
+    
+    class AsyncAccountTokenClient
+      # @return [Merge::AsyncRequestClient]
+      attr_reader :request_client
+      
+      # @param request_client [Merge::AsyncRequestClient]
+      # @return [Merge::Integrations::AsyncAccountTokenClient]
+      def initialize(request_client:)
+        @request_client = request_client
+      end
+      
+      # Returns the account token for the end user with the provided public token.
+      #
+      # @param public_token [String]
+      # @param request_options [Merge::RequestOptions]
+      # @return [Merge::Integrations::AccountToken]
+      # @example
+      #  api = Merge::Client.new(
+      #    base_url: "https://api.example.com",
+      #    environment: Merge::Environment::PRODUCTION,
+      #    api_key: "YOUR_AUTH_TOKEN"
+      #  )
+      #  api.Integrations.account_token.retrieve(public_token: "public_token")
+      def retrieve(public_token:, request_options: nil)
+        Async do
+          response = @request_client.conn.get do |req|
+            req.options.timeout = request_options.timeout_in_seconds unless request_options&.timeout_in_seconds.nil?
+            req.headers["Authorization"] = request_options.api_key unless request_options&.api_key.nil?
+            req.headers["X-Account-Token"] = request_options.account_token unless request_options&.account_token.nil?
+            req.headers = {
+              **(req.headers || {}),
+              **@request_client.get_headers,
+              **(request_options&.additional_headers || {})
+            }.compact
+            unless request_options.nil? || request_options&.additional_query_parameters.nil?
+              req.params = { **(request_options&.additional_query_parameters || {}) }.compact
+            end
+            unless request_options.nil? || request_options&.additional_body_parameters.nil?
+              req.body = { **(request_options&.additional_body_parameters || {}) }.compact
+            end
+            req.url "#{@request_client.get_url(request_options: request_options)}/Integrations/v1/account-token/#{public_token}"
+          end
+          Merge::Integrations::AccountToken.from_json(json_object: response.body)
+        end
+      end
+    end
+  end
+end
---
+++ lib/merge_ruby_client/integrations/types/account_token.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require "ostruct"
+require "json"
+
+module Merge
+  module Integrations
+    class AccountToken
+      # @return [String]
+      attr_reader :account_token
+      # @return [Merge::Integrations::AccountIntegration]
+      attr_reader :integration
+      # @return [String]
+      attr_reader :id
+      # @return [OpenStruct] Additional properties unmapped to the current class definition
+      attr_reader :additional_properties
+      # @return [Object]
+      attr_reader :_field_set
+      protected :_field_set
+      
+      OMIT = Object.new
+      
+      # @param account_token [String]
+      # @param integration [Merge::Integrations::AccountIntegration]
+      # @param id [String]
+      # @param additional_properties [OpenStruct] Additional properties unmapped to the current class definition
+      # @return [Merge::Integrations::AccountToken]
+      def initialize(account_token:, integration:, id:, additional_properties: nil)
+        @account_token = account_token
+        @integration = integration
+        @id = id
+        @additional_properties = additional_properties
+        @_field_set = { "account_token": account_token, "integration": integration, "id": id }
+      end
+      
+      # Deserialize a JSON object to an instance of AccountToken
+      #
+      # @param json_object [String]
+      # @return [Merge::Integrations::AccountToken]
+      def self.from_json(json_object:)
+        struct = JSON.parse(json_object, object_class: OpenStruct)
+        parsed_json = JSON.parse(json_object)
+        account_token = parsed_json["account_token"]
+        if parsed_json["integration"].nil?
+          integration = nil
+        else
+          integration = parsed_json["integration"].to_json
+          integration = Merge::Integrations::AccountIntegration.from_json(json_object: integration)
+        end
+        id = parsed_json["id"]
+        new(
+          account_token: account_token,
+          integration: integration,
+          id: id,
+          additional_properties: struct
+        )
+      end
+      
+      # Serialize an instance of AccountToken to a JSON object
+      #
+      # @return [String]
+      def to_json(*_args)
+        @_field_set&.to_json
+      end
+      
+      # Leveraged for Union-type generation, validate_raw attempts to parse the given
+      #  hash and check each fields type against the current object's property
+      #  definitions.
+      #
+      # @param obj [Object]
+      # @return [Void]
+      def self.validate_raw(obj:)
+        obj.account_token.is_a?(String) != false || raise("Passed value for field obj.account_token is not the expected type, validation failed.")
+        Merge::Integrations::AccountIntegration.validate_raw(obj: obj.integration)
+        obj.id.is_a?(String) != false || raise("Passed value for field obj.id is not the expected type, validation failed.")
+      end
+    end
+  end
+end
